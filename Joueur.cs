ce9ca9d1dfa1341f4ed24e4ef6fe07da0c7b6513
Tumid1968 <jadmessoudi2003@gmail.com>
2024-11-23 19:25:34 +0100

    Add files via upload

---
+++ Joueur.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boggle
+{
+    internal class Joueur
+    {
+        private string nom;
+        private long score;
+        private List<string> mots;
+        public Joueur(string n,long s,string[] m)
+        {
+            this.nom = n;
+            this.score = s;
+            this.mots = new List<string>();
+        }
+        public string Nom
+        {
+            get { return nom; }
+        }
+        public long Score
+        {
+            get { return score; }
+            set { score = value; }
+        }
+        
+        public bool Contain(string mot )
+        {
+            return mots.Contains( mot );
+        }
+        public void Add_Mot(string mot)
+        {
+            if (!Contain(mot))
+            {
+                mots.Add( mot);
+            }
+            
+        }
+        public string toString()
+        {
+            string res = "Le nom est "+Nom+" et le score est "+Score+" et les mots trouvés sont "+string.Join(", ", mots);
+            return res;
+        }
+
+
+    }
+}
