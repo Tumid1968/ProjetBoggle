21169d93e381c9567fa8857ada4dd69115cbd620
Tumid1968 <jadmessoudi2003@gmail.com>
2024-12-08 16:44:26 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boggle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Dictionnaire dictionnaireFr = new Dictionnaire("MotsPossiblesFR.txt", "Français");
+
+            Console.Write(dictionnaireFr.toString());
+
+            string mot = "tranquille";
+            if (dictionnaireFr.RechDichoRecursif(mot))
+            {
+
+                Console.WriteLine("Le mot " + mot.ToUpper() + " est dans le dictionnaire francais.");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Le mot " + mot.ToUpper() + " n'est pas dans le dictionnaire francais.");
+                Console.WriteLine() ;
+            }
+
+            Dictionnaire dictionnaireEn = new Dictionnaire("MotsPossiblesEN.txt", "Anglais");
+
+            Console.Write(dictionnaireEn.toString());
+
+            mot = "Khelil";
+            if (dictionnaireEn.RechDichoRecursif(mot))
+            {
+                Console.WriteLine("Le mot "+ mot.ToUpper() + " est dans le dictionnaire anglais.");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Le mot " + mot.ToUpper() + " n'est pas dans dictionnaire anglais.");
+                Console.WriteLine();
+            }
+
+
+
+            Console.WriteLine("Appuyez sur une touche pour quitter...");
+            Console.ReadKey();  
+
+        }
+    }
+}
