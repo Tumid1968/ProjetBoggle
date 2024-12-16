19eeee92facf8b6b83b93e72a3510cf3fa088182
Tumid1968 <jadmessoudi2003@gmail.com>
2024-12-16 17:06:18 +0100

    Add files via upload

--- Jeu.cs
+++ Jeu.cs
@@ -2,118 +2,212 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Boggle
+namespace Projet_Boggle
 {
     internal class Jeu
     {
+        private List<Joueur> joueurs;
         private Plateau plateau;
-        private List<Joueur> khelil;
-        private int tempsPassé;
-        private List<(char lettre, int score)> lettre_score;
-        private Random random;
-        public Jeu(Random random,int tempsPassé = 6)
+        private Dictionnaire dictionnaire;
+        private int durée_partie;
+        private List<int> scores;
+        private List<char> lettre;
+        private int duree_tour;
+        public Jeu()
         {
-            this.random = random;
-            this.tempsPassé=tempsPassé;
-            khelil = new List<Joueur>();
-            plateau = new Plateau(random, new Dictionnaire("Mots_PossiblesFR.txt", "fr"), new Dé[4, 4]);
-            lettre_score = donne_score_lettre("Lettres.txt");
-        }
-        private List<(char lettre, int score)> donne_score_lettre(string fichier)
-        {
-            List<(char lettre, int score)> lettres = new List<(char lettre, int score)>();
+            this.dictionnaire = null;
+            this.joueurs = new List<Joueur>();
+            this.plateau = null;
+            this.durée_partie = 2;
+            this.duree_tour = 60;
+            this.lettre = new List<char>();
+            this.scores = new List<int>();
+            string[] fichier = File.ReadAllLines("Lettres.txt");
             try
             {
-                string[] l = File.ReadAllLines(fichier);
-                foreach(string s in l)
+                foreach (string line in fichier)
                 {
-                    string[] c = s.Split(';');
-                    if(c.Length >= 2)
-                    {
-                        char le = c[0][0];
-                        int score = int.Parse(c[1]);
-                        lettres.Add((le,score));
-                    }
+                    string[] l1 = line.Split(';');
+                    lettre.Add(char.Parse(l1[0]));
+                    scores.Add(int.Parse(l1[1]));
+                    
+
                 }
             }
-            catch(FileNotFoundException f)
+            catch (IOException f)
             {
-                Console.WriteLine("Fichier introuvable"+f.Message);
+                Console.WriteLine("Le fichier n'existe pas");
+                Console.WriteLine(f.Message);
             }
-            catch (Exception e)
+        }
+        public List<int> Scores
+        {
+            get { return scores; }
+            set { scores = value; }
+        }
+        public void Configuration_du_jeu()
+        {
+            Console.WriteLine("Choisissez une langue");
+            string langue = Console.ReadLine().ToUpper();
+            dictionnaire = new Dictionnaire(langue);
+            for(int i =1; i <= 2; i++)
             {
-                Console.WriteLine("Erreur lors du chargement du message "+e.Message);
+                Console.WriteLine("Entrez le nom du joueur " + i);
+                string nom= Console.ReadLine().Trim();
+                Add_Joueur(nom, 0, new List<string>());
             }
-            return lettres;
         }
-        public void AjouterKhelil(string prenom)
+        public void Add_Joueur(string nom,int score,List<string>mots_trouvés)
         {
-            khelil.Add(new Joueur(prenom, 0, new string[] { }));
-            
+            joueurs.Add(new Joueur(nom,score,mots_trouvés));
         }
-        public void Demarrer()
+        public void démarrer_une_partie()
         {
-            Console.WriteLine("partie de Boggle commence");
-            DateTime finpartie= DateTime.Now.AddMinutes(tempsPassé);
-            int numKhelil = 0;
-            while(DateTime.Now<finpartie)
+            Console.WriteLine("La partie Boggle commence");
+            DateTime début_de_partie= DateTime.Now;
+            while ((DateTime.Now - début_de_partie).TotalMinutes < durée_partie)
             {
-                Joueur khelilactuel = khelil[numKhelil];
-                Console.WriteLine("C'est au tour de " + khelilactuel.Nom);
-                Console.WriteLine(plateau.toString());
-                DateTime fin_du_tour = DateTime.Now.AddMinutes(1);
-                while (DateTime.Now < fin_du_tour)
+                foreach(Joueur joueur in joueurs)
                 {
-                    Console.WriteLine("Ecris un mot");
-                    string mot = Console.ReadLine();
-                    if (mot.Length < 2)
+                    if ((DateTime.Now - début_de_partie).TotalMinutes >= durée_partie)
                     {
-                        Console.WriteLine("Mot invalide : doit contenir au moins 2 lettres.");
+                        break;
+                    }
+                    Console.WriteLine("\n C'est au tour de " + joueur.Nom + " de jouer");
+                    try
+                    {
+                        Console.WriteLine("Création du plateau pour ce joueur...");
+                        plateau = new Plateau(dictionnaire);
+                        Console.WriteLine("Plateau créé avec succès !");
+                        Console.WriteLine(plateau.toString());
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Erreur lors de la création du plateau "+ex.Message);
                         continue;
                     }
-                    if(!plateau.Test_Plateau(mot))
+                    List<string> mots_du_tour = new List<string>();
+                    DateTime début_du_tour = DateTime.Now;
+                    while ((DateTime.Now - début_du_tour).TotalSeconds < duree_tour)
                     {
-                        Console.WriteLine("Mot invalide : n'est pas formable à partir des dés.");
-                        continue;
-                    }
-                    if (khelilactuel.Contain(mot))
-                    {
-                        Console.WriteLine("Mot déjà trouvé.");
-                        continue;
-                    }
-                    khelilactuel.Add_Mot(mot);
-                    int ScoreM = 0;
-                    foreach(char lettre in mot.ToUpper())
-                    {
-                        foreach(var(lettre_utilisé,score) in lettre_score)
+                        if ((DateTime.Now - début_du_tour).TotalSeconds >= duree_tour)
                         {
-                            if(lettre==lettre_utilisé)
-                            {
-                                ScoreM = ScoreM + score;
-                                break;
-
-                            }
+                            Console.WriteLine("Le temps est écoulé pour " + joueur.Nom + ".");
+                            break;
                         }
+                        Console.WriteLine("Saisissez un nouveau mot trouvé");
+                        string mot = Console.ReadLine().ToUpper();
+                        if ((DateTime.Now - début_du_tour).TotalSeconds >= duree_tour)
+                        {
+                            Console.WriteLine("Le temps est écoulé pour " + joueur.Nom + ".");
+                            break;
+                        }
+                        if (mot.Length < 2)
+                        {
+                            Console.WriteLine("Le nombre minimum requis de caractères est de 2");
+                            continue;
+
+                        }
+                        if (mots_du_tour.Contains(mot.ToUpper()))
+                        {
+                            Console.WriteLine("Le joueur "+joueur.Nom+" a déjà saisis ce mot.");
+                            continue;
+                        }
+                        
+                        if(!dictionnaire.RechDichoRecursifFr1(mot.ToUpper()) && dictionnaire.Langue=="FRANCAIS")
+                        {
+                            Console.WriteLine("Le mot n'est pas trouvé dans le dictionnaire");
+                            continue;
+                        }
+                        else if(dictionnaire.Langue=="ANGLAIS"&& !dictionnaire.RechDichoRecursifEn1(mot.ToUpper()))
+                        {
+                            Console.WriteLine("Le mot n'est pas trouvé dans le dictionnaire");
+                            continue;
+
+
+                        }
+                        if (!plateau.Test_Plateau(mot))
+                        {
+                            Console.WriteLine("Le mot est invalide sur le plateau");
+                            continue;
+                        }
+                        mots_du_tour.Add(mot);
+                        int score_du_mot=calculer_score_mot(mot);
+                        joueur.Score=joueur.Score+score_du_mot;
+                        joueur.Add_Mot(mot);
+                        afficher_score_de_mots(joueur);
                     }
-                    khelilactuel.Score = khelilactuel.Score + ScoreM;
-                    Console.WriteLine("Mot validé : "+mot+" et son score est : "+ScoreM);
-
-
                 }
-                numKhelil=(numKhelil+1)%khelil.Count;
             }
-            Console.WriteLine("Le temps est fini, vous pouvez donc voir les résultats ci-dessous");
-            foreach(Joueur j in khelil)
+            Fin_de_partie();
+        }
+        private void afficher_score_de_mots(Joueur j)
+        {
+            Console.WriteLine("Le score de " + j.Nom + " est de " + j.Score + " grâce aux mots cités suivants");
+            foreach(string m in j.Mots_Trouves)
             {
-                Console.WriteLine(j.toString());
+                Console.Write(m+" ");
+            }Console.WriteLine();
+        }
+        private int calculer_score_mot(string mot)
+        {
+            int score = 0;
+            foreach(char l in mot)
+            {
+                int i = lettre.IndexOf(l);
+                if (i != -1)
+                {
+                    score = score + Scores[i];
+                }
+            }
+            return score;
+                
+        }
+        private void GenererNuagePourJoueurs()
+        {
+            foreach (Joueur joueur in joueurs)
+            {
+                Console.WriteLine($"Génération du nuage de mots pour {joueur.Nom}...");
+
+                // Créer un dictionnaire mot -> score
+                Dictionary<string, int> motsScores = joueur.Mots_Trouves
+                    .ToDictionary(mot => mot, mot => calculer_score_mot(mot));
+
+                // Générer le nuage de mots
+                string cheminFichier = $"{joueur.Nom}_NuageDeMots.png";
+                NuageDeMots nuage = new NuageDeMots(joueur.Mots_Trouves, motsScores);
+                nuage.GenererNuage(cheminFichier);
             }
         }
 
+        private void Fin_de_partie()
+        {
+            Console.WriteLine("\n La partie est terminée");
 
+            for(int i=0; i<joueurs.Count; i++)
+            {
+                Joueur joueur = joueurs[i];
+                Console.WriteLine(joueur.Nom+" a un score de "+joueur.Score);
+            }
+            Joueur Le_joueur_gagnant = calculer_le_gagnant();
+            Console.WriteLine("Le gagnant de cette partie est "+Le_joueur_gagnant.Nom+" avec "+Le_joueur_gagnant.Score+" points.");
+            GenererNuagePourJoueurs();
 
+        }
+        private Joueur calculer_le_gagnant()
+        {
+            Joueur le_joueur_gagnant = joueurs[0];
+            for(int i = 1; i < joueurs.Count; i++)
+            {
+                if (joueurs[i].Score > le_joueur_gagnant.Score)
+                {
+                    le_joueur_gagnant= joueurs[i];
+                }
+            }
+            return le_joueur_gagnant;
+        }
     }
 }
