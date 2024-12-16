3ace17328e88eb1483ad442465653bc5294ea9b7
Tumid1968 <jadmessoudi2003@gmail.com>
2024-12-16 17:06:48 +0100

    Add files via upload

--- Plateau.cs
+++ Plateau.cs
@@ -1,91 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Boggle
+namespace Projet_Boggle
 {
     internal class Plateau
     {
-        private Dé[,] des;
-        private Dictionnaire dictionnaire;
-        private Random random;
-
-        public Plateau(Random random, Dictionnaire dictionnaire, Dé[,] des)
+        private Dé[,] ensemble_dés;
+        private Dictionnaire dictionnaire; 
+        public Plateau(Dictionnaire dict)
         {
-
-            this.dictionnaire = dictionnaire;
-            this.des = new Dé[4, 4];
-            this.random = new Random();
-
-            for (int i = 0; i < 4; i++)
+            Console.WriteLine("Création d'un nouveau plateau");
+            dictionnaire = dict;
+            ensemble_dés = new Dé[4, 4];
+            Random r = new Random();
+            for(int i =0; i<4; i++)
             {
-                for (int j = 0; j < 4; j++)
+                for(int j =0; j<4; j++)
                 {
-                    this.des[i, j] = new Dé("Lettres.txt");
-                    this.des[i, j].Lance(random);
+                    ensemble_dés[i, j] = new Dé();
+                    ensemble_dés[i, j].Lance(r);
                 }
             }
-        }
 
-        public string toString()
-        {
-            string result = "";
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    result += des[i, j].FaceVisible + " ";
-                }
-                result += "\n";
-            }
-            return result;
         }
-
         public bool Test_Plateau(string mot)
         {
-            if (string.IsNullOrEmpty(mot)) return false;
-            if (!dictionnaire.RechDichoRecursif(mot)) return false;
-
-            bool[,] visited = new bool[4, 4];
+            if(dictionnaire.Langue == "FRANCAIS")
+            {
+                if (!dictionnaire.RechDichoRecursifFr1(mot.ToUpper()))
+                {
+                    return false;
+                }
+            }
+            else 
+            {
+                if (!dictionnaire.RechDichoRecursifEn1(mot.ToUpper()))
+                {
+                    return false;
+                }
+            }
+            bool[,] case_visitée = new bool[4, 4];
             for (int i = 0; i < 4; i++)
             {
                 for (int j = 0; j < 4; j++)
                 {
-                    if (des[i, j].FaceVisible == mot[0] &&
-                        TestRecursif(mot, 0, i, j, visited))
+                    if (ensemble_dés[i, j].Face_visible[0] == mot[0] &&
+                        TestRecursif(mot.ToUpper(), 0, i, j, case_visitée))
                     {
                         return true;
                     }
+                    case_visitée = new bool[4, 4];
+
                 }
             }
             return false;
         }
-
-        private bool TestRecursif(string mot, int index, int x, int y, bool[,] visited)
+        private bool TestRecursif(string mot, int index, int x, int y, bool[,] case_visitée)
         {
             if (index == mot.Length) return true;
 
-            if (x < 0 || x >= 4 || y < 0 || y >= 4 || visited[x, y]) return false;
+            if (x < 0 || x >= 4 || y < 0 || y >= 4 || case_visitée[x, y]) return false;
 
-            if (des[x, y].FaceVisible != mot[index]) return false;
+            if (ensemble_dés[x, y].Face_visible[0] != mot[index]) return false;
 
-            visited[x, y] = true;
+            case_visitée[x, y] = true;
 
             int[] dx = { -1, -1, -1, 0, 0, 1, 1, 1 };
             int[] dy = { -1, 0, 1, -1, 1, -1, 0, 1 };
 
             for (int i = 0; i < 8; i++)
             {
-                if (TestRecursif(mot, index + 1, x + dx[i], y + dy[i], visited))
+                if (TestRecursif(mot, index + 1, x + dx[i], y + dy[i], case_visitée))
                 {
                     return true;
                 }
             }
 
-            visited[x, y] = false;
+            case_visitée[x, y] = false;
             return false;
+        }
+        public string toString()
+        {
+            string plateau_résultat = "";
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    plateau_résultat += ensemble_dés[i, j].Face_visible + " ";
+                }
+                plateau_résultat += "\n";
+            }
+            return plateau_résultat;
         }
     }
 }
