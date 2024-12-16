5ac47d499b39700e354aa26d56c40d8c8d9d1940
Tumid1968 <jadmessoudi2003@gmail.com>
2024-12-16 20:03:59 +0100

    Add files via upload

--- Joueur.cs
+++ Joueur.cs
@@ -11,11 +11,14 @@ namespace Projet_Boggle
         private string nom;
         private int score;
         private List<string> mots_Trouves;
+        private List<string> mots_Trouves_pour_Nuage;
+
         public Joueur(string n, int s, List<string> m)
         {
             this.nom = n;
             this.score = s;
-            this.mots_Trouves = m;
+            this.mots_Trouves = new List<string>();
+            this.mots_Trouves_pour_Nuage = new List<string>(mots_Trouves);
         }
         public string Nom
         {
@@ -31,16 +34,23 @@ namespace Projet_Boggle
             get { return mots_Trouves; }
             set { mots_Trouves = value; }
         }
+        public List<string> Mots_Trouves_pour_Nuage
+        {
+            get { return mots_Trouves_pour_Nuage; } 
+        }
         public bool Contain(string mot) {
             return Mots_Trouves.Contains(mot.ToUpper());  
         }
         public void Add_Mot(string mot)
         {
-            if(!Contain(mot))
+            mots_Trouves_pour_Nuage.Add(mot.ToUpper()); // Ajoute toujours à la liste des originaux
+
+            if (!Contain(mot)) // Ajoute aux mots uniques seulement si absent
             {
-                Mots_Trouves.Add(mot);
+                mots_Trouves.Add(mot.ToUpper());
             }
         }
+       
         public string toString()
         {
             string joueur_résultat = "le score de "+Nom+" est de "+Score+" grâce aux mots cités suivants"+string.Join(" ",Mots_Trouves);
