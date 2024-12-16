30b4f286c07ee16d83da8bd51e3f3975849024c4
Tumid1968 <jadmessoudi2003@gmail.com>
2024-12-16 17:06:04 +0100

    Add files via upload

--- Dé.cs
+++ Dé.cs
@@ -1,104 +1,117 @@
 ﻿using System;
-using System.IO;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
 
-namespace Boggle
+namespace Projet_Boggle
 {
     internal class Dé
     {
-        private List<char> faces;
-        private List<(char let, int NbreDeFois)> lettreNombre;
-        private char faceVisible;
-        public Dé(string fichier)
+        private static List<string> lettresTotal;
+        private static List<int> occurences;
+        private static List<int> occurences_non_modifiées;
+        private string face_visible;
+        private static int compteur_création_dés;
+        private List<string> dé_obtenu;
+
+        public Dé()
         {
-            faces = new List<char>();
-            lettreNombre = new List<(char let, int NbreDeFois)>();
+            
+            this.dé_obtenu = new List<string>();
+
+
+            string[] fichier = File.ReadAllLines("Lettres.txt");
+            if(lettresTotal == null|| occurences==null|| occurences_non_modifiées==null ) {
+                lettresTotal = new List<string>();
+                occurences_non_modifiées = new List<int>();
+                occurences = new List<int>();
+                try
+                {
+                    foreach (string line in fichier)
+                    {
+                        string[] l1 = line.Split(';');
+                        lettresTotal.Add(l1[0]);
+                        occurences.Add(int.Parse(l1[2]));
+                        occurences_non_modifiées.Add(int.Parse(l1[2]));
+
+                    }
+                }
+                catch (IOException f)
+                {
+                    Console.WriteLine("Le fichier n'existe pas");
+                    Console.WriteLine(f.Message);
+                }
+            }
+            if(compteur_création_dés>=16)
+            {
+                reinitialiser_occurences();
+            }
+            
+            
+            
+            
             Random r = new Random();
-            try
+            
+            while(dé_obtenu.Count<6)
             {
-                string[] ligne = File.ReadAllLines(fichier); // Ca ca lit le fichier et ca donne quoi comme info?
-                foreach (string l in ligne)
+                int Aleatoire2= r.Next(0, lettresTotal.Count);
+                
+                if (occurences[Aleatoire2]>0)
                 {
-                    string[] compo = l.Split(';');
-                    if (compo.Length==3)
-                    {
-                        char le = compo[0][0];
-                        int NbreDeFois = int.Parse(compo[2]);
-                        lettreNombre.Add((le, NbreDeFois));
-                    }
+                    dé_obtenu.Add(lettresTotal[Aleatoire2]);
+                    occurences[Aleatoire2] -= 1;
                 }
-                List<char> fA = new List<char>();
-                while (fA.Count < 6 && lettreNombre.Count>0)
-                {
-                    int i = r.Next(lettreNombre.Count); 
-                    var (lettrePr, Restants) = lettreNombre[i];
-
-
-                    if (Restants > 0)
-                    {
-                        fA.Add(lettrePr);
-                        lettreNombre[i] = (lettrePr, Restants - 1);
-                        if (lettreNombre[i].NbreDeFois ==0)
-                        {
-                            lettreNombre.RemoveAt(i);
-                        }
-                    }
-                }
-
-                faces = fA;
-                if (faces.Count != 6)
-                {
-                    Console.WriteLine("Le fichier doit contenir exactement 6 lettres");
-                }
-
+                
             }
-            catch (FileNotFoundException f) 
-            {
-                Console.WriteLine("Le fichier n'existe pas " + f.Message);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            compteur_création_dés = compteur_création_dés + 1;
+            Lance(r);
+            
+
+
+
 
         }
-        public char FaceVisible
-        {
-            get { return faceVisible; }
 
-        }
-        public List<char> Faces
+        
+        private static void reinitialiser_occurences()
         {
-            get { return faces; }
-            set { faces = value; }
-        }
-        public List<(char let, int NbreDeFois)> LettreNombre
-        {
-            get { return lettreNombre; }
-            set { lettreNombre = value; }   
-        }
-
-        public void Lance (Random r)
-        {
-            if(faces.Count == 6)
+            for(int i=0; i<occurences.Count; i++)
             {
-                int LettreAleat = r.Next(0, faces.Count);
-                faceVisible = faces[LettreAleat];
-
+                occurences[i] = occurences_non_modifiées[i];
             }
-            else
-            {
-                Console.WriteLine("erreur");
-            }
+            compteur_création_dés = 0;
+        }
+        
+        public List<int> Occurences
+        {
+            get { return occurences; }
+        }
+        public string Face_visible
+        {
+            get { return face_visible; }
+            set { face_visible = value; }
+        }
+        public List<string> Dé_obtenu
+        {
+            get { return dé_obtenu; }
+        }
+        public List<string> LettresTotal
+        {
+            get { return lettresTotal; }
+        }
 
+        public void Lance(Random r)
+        {           
+            int Aleatoire3 = r.Next(0,dé_obtenu.Count);
+            face_visible = dé_obtenu[Aleatoire3];           
         }
         public string toString()
         {
-            string résultat = " Le Dé avec faces : " + string.Join(", ", faces) + " et la face visible est " + faceVisible;
-            return résultat;
+            string dé_résultat = "Le Dé obtenu est " + string.Join(" ", dé_obtenu) + " et la face visible de ce Dé est " + face_visible;
+            return dé_résultat;
         }
     }
 }
